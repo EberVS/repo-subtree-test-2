775ae48b7c18f04c6014d54faa75bdb472af1302
Eber Valdez <evaldezs@itron.com>
2024-03-20 16:45:03 -0600

    1-commit

---
+++ Subtree4Test2/src/main/java/com/example/subtree4test2/Subtree4Test2Application.java
@@ -0,0 +1,13 @@
+package com.example.subtree4test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Subtree4Test2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Subtree4Test2Application.class, args);
+	}
+
+}
---
+++ Subtree4Test2/src/test/java/com/example/subtree4test2/Subtree4Test2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subtree4test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Subtree4Test2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
